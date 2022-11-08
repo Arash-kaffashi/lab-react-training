e0793f020c73a19ff3e1c0fa498291c8c3e17035
Arash Cipriano Cardoso Kaffashi <arash_kaffashi@hotmail.com>
2022-11-08 19:38:10 -0400

    done: Iteration 10

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Rating from './components/Rating';
 import DriverCard from './components/DriverCard';
 import LikeButton from './components/LikeButton';
 import ClickablePicture from './components/ClickablePicture';
+import Dice from './components/Dice';
 
 function App() {
   return (
@@ -97,6 +98,7 @@ function App() {
       <LikeButton />
       <LikeButton />
       <ClickablePicture img="maxence.png" imgClicked="maxence-glasses.png" />
+      <Dice />
     </div>
   );
 }
