834765e19fb261d9c7f0cb0748c75d6786031eaa
Arash Cipriano Cardoso Kaffashi <arash_kaffashi@hotmail.com>
2022-11-08 22:13:32 -0400

    done: Iteration 12

---
+++ src/components/NumbersTable.js
@@ -0,0 +1,7 @@
+export default function NumbersTable({ limit }) {
+  let result = [];
+  for (let i = 0; i < limit; i++) {
+    result.push(<div key={i}>{i + 1}</div>);
+  }
+  return <div className="numbers-table">{result}</div>;
+}
